b579a6794a5751cc3a90730bfbe1496f1d7b8d45
clementh59 <hecquet.c59@gmail.com>
2023-12-13 16:26:31 +0100

    test-1

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const MAXIMAL_ID_LENGTH = 100;
 
 export const HANDLE_IMG_THRESHOLD = 0.2;
 
-export const REFRESH_ICON_INTERVAL = 20;
+export const REFRESH_ICON_INTERVAL = 5000;
 
 export const MIN_DISTANCE_FROM_LEFT_BORDER_IN_PIXELS = 365;
 
