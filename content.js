b25edcd48846f0769d9b1f3485430484d924ce45
PercheEtienne <e.perche59@gmail.com>
2024-05-01 15:06:55 +0200

    display error

--- content.js
+++ content.js
@@ -36,6 +36,9 @@ document.addEventListener('contextmenu', (event) => {
 chrome.runtime.onMessage.addListener((request) => {
   if (request.type === MSG_GET_HTML_COMPONENT) {
     const nearestMedia = findNearestMedia(clickedEl);
+    if(!nearestMedia){
+      displayError('Unable to locate a media to verify.');
+    }
     if (nearestMedia.type === 'video') {
       debug('Video found', nearestMedia.element);
       handleSingleVideo(nearestMedia.element, singleImageVerification);
